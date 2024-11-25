8efa7ad6f33fde7c52d1bf39ce7e3d3695ecf10c
alexisCode50 <skrillex.ja14@gmail.com>
2024-11-25 11:55:43 -0600

    setup react router

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,18 @@
 import * as React from 'react'
+import { HashRouter, Routes, Route } from "react-router"
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import HomePage from './ui/pages/HomePage.jsx'
+import DashboardPage from './ui/pages/DashboardPage.jsx'
+import ProfilePage from './ui/pages/ProfilePage.jsx'
 import './index.css'
 
 const root = createRoot(document.body)
-root.render(<App />)
+root.render(
+    <HashRouter>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+    </HashRouter>
+)
---
+++ src/ui/pages/HomePage.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { NavLink } from "react-router"
+
+export default function HomePage() {
+  return (
+    <section className="bg-gray-50">
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            Template
+            <strong className="font-extrabold text-blue-700 sm:block">Electron + React</strong>
+          </h1>
+          <p className="mt-4 sm:text-xl/relaxed">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+            numquam ea!
+          </p>
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <NavLink
+              to="/dashboard"
+              className="block w-full rounded bg-blue-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+            >
+              Dashboard
+            </NavLink>
+            <NavLink
+              to="/profile"
+              className="block w-full rounded px-12 py-3 text-sm font-medium text-blue-600 shadow hover:text-blue-700 focus:outline-none focus:ring active:text-blue-500 sm:w-auto"
+            >
+              Profile
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
